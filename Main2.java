7bb649c95212ceb0ef764cc239186ea79149026c
nairx <nairxprv@gmail.com>
2026-01-07 20:59:27 +0530

    updated

--- Main2.java
+++ Main2.java
@@ -5,26 +5,3 @@ public class Main2{
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// public class Main2 {
-// public static void main(String[] args){
-// int a=10;
-// double rating=4.5;
-// final int max=100;
-// System.out.println(a);
-// System.out.println(rating);
-// System.out.println(max);
-// }
-// }
