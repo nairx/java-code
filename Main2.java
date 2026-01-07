d6866abdabe6e7f15bf5169cf463fa656a2735fa
nairx <nairxprv@gmail.com>
2026-01-07 20:57:55 +0530

    first commit

---
+++ Main2.java
@@ -0,0 +1,30 @@
+public class Main2{
+    public static void main(String[] args){
+        int a = 10;
+        System.out.println(a);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class Main2 {
+// public static void main(String[] args){
+// int a=10;
+// double rating=4.5;
+// final int max=100;
+// System.out.println(a);
+// System.out.println(rating);
+// System.out.println(max);
+// }
+// }
---
+++ Main3.java
@@ -0,0 +1,12 @@
+public class Main3 {
+    public static void main(String[] args) {
+        // Operators: +, -, *, /, ==, !=, &&, ||
+        System.out.println(12 + 34);
+        System.out.println(24 % 2);
+        System.out.println(34 == 23);
+        System.out.println(34 != 23);
+        System.out.println((13 > 5) && (5 > 2));
+        System.out.println((3 > 5) || (5 > 2));
+
+    }
+}
