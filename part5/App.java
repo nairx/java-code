ee90eb300fc4ad45e0cbe198e3082c79de4552b2
nairx <nairxprv@gmail.com>
2026-01-19 20:38:48 +0530

    updated

---
+++ part5/App.java
@@ -0,0 +1,26 @@
+package part5;
+
+class Calc {
+    int a;
+    int b;
+    public Calc(int n1, int n2) {
+        this.a = n1;
+        this.b = n2;
+    }
+    void add(){
+        System.out.println(this.a + this.b);
+    }
+
+    void multiply(){
+        System.out.println(this.a * this.b);
+    }
+
+}
+
+public class App {
+    public static void main(String[] args) {
+        Calc c = new Calc(4, 5);
+        c.add();
+        c.multiply();
+    }
+}
